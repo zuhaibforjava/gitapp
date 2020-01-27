eba922f1962dd27fe389fdbea6a1c59ace53f0ca
Zuhaib Khan <Zuhaib Khan@DESKTOP-RRVTV2J>
2020-01-27 12:40:17 +0530

    ist done

--- src/gitapp/test/GitTest.java
+++ src/gitapp/test/GitTest.java
@@ -3,7 +3,8 @@ package gitapp.test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("WELCOME TO GIT HUB");
 
 	}
 
